bdb5e162aae300ceeee6cb85f2aa7404230999df
José Luiz Rodrigues <zeluizr@MacBook-Pro-de-Jose.local>
2020-01-30 17:57:36 -0300

    scripts

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-// A $( document ).ready() block.
 $(document).ready(function() {
   $(".helperComplement").remove();
   const Element = $(".vitrine .shelf > h2");
@@ -9,22 +8,9 @@ $(document).ready(function() {
     .text(Split[0]);
   Element.html("");
   Element.append(Link);
-  // http://tutienda.myvtex.com/
-  // buscapagina
-  // ID de la vitrina
-  // ?sl=481c7748-cdc5-44f0-93c2-6ecf4cccc4ee
-  // Cantidad de Items en la vitrina
-  // &PS=7
-  // Cantidad de ul en el listado de productos
-  // &cc=7
-  // Cantidad de ul en el listado de productos
-  // &sm=0
-  // Paginacion
-  // &PageNumber=3
-  // Puedes agregar Colecciones, Marcas y Categorias
-  // &fq=H:138
+
   $.get(
-    "/buscapagina?sl=19ccd66b-b568-43cb-a106-b52f9796f5cd&PS=10&cc=10&sm=0&PageNumber=1&fq=H:1142&tt=Las mejores ofertas",
+    "/buscapagina?sl=19ccd66b-b568-43cb-a106-b52f9796f5cd&PS=10&cc=10&sm=1&PageNumber=1&fq=H:1142&tt=Las mejores ofertas",
     function(data) {
       $("#app").append(data);
       $(".helperComplement").remove();
